584250339dbd84c99e0866a7cb3a1fe7f28e8ddf
eliteinovastaticwebsite-crypto <sabarisoftwareinstitute25@gmail.com>
2026-02-03 16:42:15 +0530

    first commit

--- src/pages/VendorProfile.jsx
+++ src/pages/VendorProfile.jsx
@@ -639,7 +639,7 @@ const VendorProfile = () => {
                   <svg className="w-4 h-4 md:w-5 md:h-5 mr-2 text-red-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                   </svg>
-                  Our Office Contact
+                  Our Office Contact For Enquiry
                 </h3>
                 <div className="space-y-2">
                   <div className="flex items-center gap-2">
